077f3703161f4bb45a5bba0aa004c9d1da95003d
Kendyl Hansen <krhansen2016@gmail.com>
2026-02-11 01:02:14 -0700

    importing all my css files

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
-import "./login/login.css"
+import "./login/login.css";
 
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { Login } from "./login/login";
--- src/create/create.jsx
+++ src/create/create.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./create.css";
 
 export function Create() {
     return (
