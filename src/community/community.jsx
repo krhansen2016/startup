b21b6e9712abf6d6a4e3370472802f21f9b681a8
Kendyl Hansen <krhansen2016@gmail.com>
2026-02-10 23:07:54 -0700

    Created the react components for all the html pages

---
+++ src/community/community.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Community() {
+    return (
+        <main className="container-fluid bg-secondary text-center">
+            <div>community displayed here</div>
+        </main>
+    )
+}
