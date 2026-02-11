617ede81927bf63a83ab4d2f5b83ca3f6661c08e
Kendyl Hansen <krhansen2016@gmail.com>
2026-02-10 17:28:26 -0700

    Copied over my login html to my react app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
 export default function App() {
-    return <div className="body bg-dark text-light">App will display here</div>
+    return <div className="body bg-dark text-light"><header>
+            <h1 class="heading" id="login-header">Concept Threads</h1>
+        </header>
+        <main id="login">
+            <div class="login-box">
+            <h1 class="heading">Login</h1>
+            <form method="get" action="create.html">
+                <div class="user-box">
+                    <input type="text" name="email" required />
+                    <label>Email</label>
+                </div>
+                <div class="user-box">
+                    <input type="password" name="password" required />
+                    <label>Password</label>
+                </div>
+                <div class="buttons">
+                    <button type="submit">Login to Existing Account</button>
+                    <button type="submit">Create New Account</button>
+                </div>
+            </form>
+            </div>
+        </main>
+        <footer>
+            <div>Created by: Kendyl Hansen</div>
+            <div>Find my GitHub Repository <a id="github-link"href="https://github.com/krhansen2016/startup">here</a>!</div>
+        </footer></div>
 }
