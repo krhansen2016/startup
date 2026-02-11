3b7a4cc2be33df75cced558b005c3e80b70da876
Kendyl Hansen <krhansen2016@gmail.com>
2026-02-10 23:15:30 -0700

    adding in Browser router

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 import "./login/login.css"
 
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+
 export default function App() {
-    return <div className="app">
-            <header>
-                <h1 className="heading" id="login-header">Concept Threads</h1>
-            </header>
-            <main id="login">App components go here</main>
-            <footer>
-                <div>Created by: Kendyl Hansen</div>
-                <div>Find my GitHub Repository <a id="github-link"href="https://github.com/krhansen2016/startup">here</a>!</div>
-            </footer>
-        </div>
+    return (
+        <BrowserRouter>
+            <div className="app">
+                <header>
+                    <h1 className="heading" id="login-header">Concept Threads</h1>
+                </header>
+                <main id="login">App components go here</main>
+                <footer>
+                    <div>Created by: Kendyl Hansen</div>
+                    <div>Find my GitHub Repository <a id="github-link"href="https://github.com/krhansen2016/startup">here</a>!</div>
+                </footer>
+            </div>
+        </BrowserRouter>
+    )
 }
