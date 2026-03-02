ac64dded70f7da0d59b145378d9c84ee780a6e01
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-01 22:40:40 -0700

    changed login inputs and created func handleLogin

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 export function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLogin(event) {
+        event.preventDefault();
+        localStorage.setItem("userName", email);
+    }
+
     return (
         <main id="login">
             <div className="login-box">
             <h1 className="heading">Login</h1>
-            <form method="get" action="create.html">
+            <form onSubmit={handleLogin}>
                 <div className="user-box">
-                    <input type="text" name="email" required />
+                    <input type="text" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
                     <label>Email</label>
                 </div>
                 <div className="user-box">
-                    <input type="password" name="password" required />
+                    <input type="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
                     <label>Password</label>
                 </div>
                 <div className="buttons">
