90f0be6252e683012fc957f24d8c3f0b2eae98d8
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-11 15:56:37 -0600

    added in setAuthCookie func from simon example

--- service/index.js
+++ service/index.js
@@ -38,6 +38,15 @@ async function createUser(email, password) {
   return user;
 }
 
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
