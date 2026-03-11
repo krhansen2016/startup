71c45d9fbb28391e90060430012ca360802f3a37
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-11 15:44:38 -0600

    created basic layout for index.js

--- service/index.js
+++ service/index.js
@@ -1,2 +1,21 @@
+const cookieParser = require('cookie-parser');
+const bycrypt = require('bcryptjs');
+const express = require('uuid')
+const app = express();
+
+const authCookieName = 'token';
+
+
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
 app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.json());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
