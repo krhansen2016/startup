bcbd0a5842ef10816c9c327e136540a0126c0a31
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-17 15:53:25 -0600

    fixed my login box heading

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -16,6 +16,7 @@ export function Login({ username, authState, onAuthChange }) {
   return (
     <main id="login">
       <div className="login-box">
+        <h1 className="heading">Login</h1>
         {authState === AuthState.Unauthenticated && (
           <Unauthenticated username={username} onLogin={(loginUserName) => onAuthChange(loginUserName, AuthState.Authenticated)} />
         )}
