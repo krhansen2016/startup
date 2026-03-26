f54f34cd122b23b9e49116a6c664a5b3113c039b
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-26 16:39:41 -0600

    added in more mongo const and initializing collections

--- service/database.js
+++ service/database.js
@@ -1,6 +1,10 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('conceptthreads');
+const usercollection = db.collection('user')
+const designcollection = db.collection('design')
 
 (async function testConnection() {
   try {
