f4b2fd72cc9777cb206869da6d03caf1bb92883c
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-26 17:07:15 -0600

    updated listening code to match simon-db's

--- service/index.js
+++ service/index.js
@@ -188,6 +188,6 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-app.listen(port, () => {
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
