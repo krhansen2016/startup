4f45546314fed85eeaeace57f2863f71cd9a1af0
Kendyl Hansen <krhansen2016@gmail.com>
2026-03-26 15:06:15 -0600

    created database js file

---
+++ service/database.js
@@ -0,0 +1,12 @@
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('simon');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();created 
