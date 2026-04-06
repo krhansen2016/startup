9507b1dc76a8cf5cfc51be0429d9afd50af9891d
Kendyl Hansen <krhansen2016@gmail.com>
2026-04-05 19:58:02 -0600

    created frontend Websocket helper

---
+++ src/community/communityNotifier.js
@@ -0,0 +1,29 @@
+class CommunityNotifier {
+    handlers = [];
+
+    constructor() {
+        const protocol = window.location.protocol === 'http: ' ? 'ws' : 'wss';
+        const host = window.location.host;
+        this.socket = new WebSocket(`${protocol}://${host}/ws`);
+
+        this.socket.onmessage = async (msg) => {
+            try {
+                const event = JSON.parse(await msg.data.text());
+                this.handlers.forEach((handler) => handler(event));
+            } catch (err) {
+                console.error('WebSocket message error:', err);
+            }
+        };
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers = this.handlers.filter((h) => h !== handler);
+    }
+}
+
+const CommunityNotifier = new CommunityNotifier();
+export { CommunityNotifier };
