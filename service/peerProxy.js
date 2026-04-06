cbebd36b80b5257afcc744bd988e070ca2958485
Kendyl Hansen <krhansen2016@gmail.com>
2026-04-05 18:50:14 -0600

    forwarding messages to everyone except sender

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -5,5 +5,13 @@ function peerProxy(httpServer) {
 
     socketServer.on('connection', (socket) => {
         socket.isAlive = true;
+
+        socket.on('message', (data) => {
+            socketServer.clients.forEach((client) => {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {client.send(data.toString());}
+            });
+        });
+
+        
     })
 }
